b7b3c0ca2ced43fb0587961b1721ed29f704d7f4
Borja Sanz Gresa <borjasanz11@gmail.com>
2021-10-20 22:49:15 +0200

    Second exercise - WIP

--- index.js
+++ index.js
@@ -2,23 +2,64 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = {}; //Stores all the usernames. The index is each socket's id
+
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/login.html');
 });
 
+app.post('/index.html', function(req, res){
+	res.sendFile(__dirname + '/login.html');
+});
+
+/* app.get('/', function(req, res){
+  var username = req.query.nickname;
+  res.sendFile(__dirname + '/index.html');
+}); */
+
 io.on('connection', function(socket){
-  /* 1. Notificar la conexión de un nuevo usuario */
+  socket.on('login', (name) => {
+    console.log(users);
+    if (Object.values(users).includes(name)) {
+      socket.emit("login", null);
+    } else {
+      users[socket.id] = name;
+      console.log("logged in " + name);
+      socket.emit('login', name);
+    }
+  });
+
+  /* 
+  1. User connects
+  2. Support for nicknames
+  */
+  /* socket.on('login', name => {
+    users[socket.id] = name;
+  }); */
   socket.on('new user', nickname => {
     console.log('User ' + nickname + ' joined');
     io.emit('new user', nickname);
   });
   console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+
+  /* 
+  1. User disconnects
+  2. Support for nicknames
+  */
+  socket.on('disconnect', nickname => {
+    console.log('User ' + nickname + ' disconnected');
+    io.emit('user disconnected', nickname);
   });
+  console.log('a user disconnected');
+
+  /* Chat msg */ 
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    let data = {
+      //from: users[socket.id],
+      message: msg,
+    }
+    console.log('message: ' + data);
+    io.emit('chat message', data);
   });
 });
 
